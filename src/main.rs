7a08c6ee5333ca3a4d5c42e48ac22ca6bf364b71
向越可 <xiangyk@dcxs100.com>
2015-12-08 20:41:56 +0800

    Add implementations for ::std::ops::*

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::*;
+
 use std::fmt;
 
 fn main() {
@@ -16,6 +16,12 @@ impl<T: fmt::Display> fmt::Display for Lazy<Fn()->T> {
 }
 
 struct Lazy<T: ?Sized>(Box<T>);
+
+impl<F> Lazy<F> {
+    fn by<T>(f: F) -> Self where F: Fn()->T {
+        Lazy(Box::new(f))
+    }
+}
 
 impl<T: Clone + 'static> Lazy<Fn()->T> {
     pub fn new(v: T) -> Self {
@@ -35,9 +41,33 @@ impl<T: Clone + 'static> ToLazy for T {
     }
 }
 
-impl<L, R> Add<Lazy<Fn()->R>> for Lazy<Fn()->L> where L: Clone + Add<R>, R: Clone {
-    type Output = Lazy<Fn() -> <L as Add<R>>::Output>;
-    fn add(self, rhs: Lazy<Fn()->R>) -> Self::Output {
-        Lazy(Box::new(move || self.0() + rhs.0()))
+impl<T, V> ::std::ops::Not for Lazy<T>
+where T: Fn()->V + 'static, V: ::std::ops::Not {
+    type Output = Lazy<Fn() -> <V as ::std::ops::Not>::Output>;
+    fn not(self) -> Self::Output {
+        Lazy(Box::new(move || !self.0()))
     }
 }
+
+macro_rules! impl_bin_op {
+    ($name:ident, $method:ident) => {
+        impl<L, R> ::std::ops::$name<Lazy<Fn()->R>> for Lazy<Fn()->L>
+        where L: Clone + ::std::ops::$name<R>, R: Clone {
+            type Output = Lazy<Fn() -> <L as ::std::ops::$name<R>>::Output>;
+            fn $method(self, rhs: Lazy<Fn()->R>) -> Self::Output {
+                use ::std::ops::$name;
+                Lazy(Box::new(move || $name::$method(self.0(), rhs.0())))
+            }
+        }
+    }
+}
+
+impl_bin_op!{Add, add}
+impl_bin_op!{BitAnd, bitand}
+impl_bin_op!{BitOr, bitor}
+impl_bin_op!{BitXor, bitxor}
+impl_bin_op!{Mul, mul}
+impl_bin_op!{Rem, rem}
+impl_bin_op!{Shl, shl}
+impl_bin_op!{Shr, shr}
+impl_bin_op!{Sub, sub}
